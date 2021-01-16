296eeeb317c634702ca0aab5474aef69be2d3aac
mHaisham <mHaisham@users.noreply.github.com>
2021-01-16 08:44:42 +0500

    Refactored rollup config

--- qrshare/client/rollup.config.js
+++ qrshare/client/rollup.config.js
@@ -33,6 +33,48 @@ function serve() {
 	};
 }
 
+function create({name, flask, singleBundle}) {
+	return {
+		input: `src/${name}/${name}.js`,
+		output: {
+			sourcemap: true,
+			format: 'iife',
+			name: 'app',
+			file: `public/build/${name}/bundle.js`
+		},
+		plugins: [
+			svelte({
+				compilerOptions: {
+					dev: !production
+				}
+			}),
+			css({ output: `bundle.css` }),
+	
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
+			commonjs(),
+	
+			// In dev mode, call `npm run start` once
+			// In flask mode, dont serve
+			// the bundle has been generated
+			!production && !flask && serve(),
+			
+			!production && (singleBundle
+				? livereload(`public`)
+				: livereload(`public/build/${name}`)),
+	
+			// If we're building for production (npm run build
+			// instead of npm run dev), minify
+			production && terser()
+		],
+		watch: {
+			clearScreen: false
+		},
+	}
+}
+
 export default (args) => {
 	// choosing bundles
 	for (const name of bundles) {
@@ -44,99 +86,7 @@ export default (args) => {
 
 	const singleBundle = bundles.length === 1
 	const flask = args['config-flask'] === true
-	const configs = []
-
-	// create configs
-	for (const name of bundles) {
-		let inst = {
-			input: `src/${name}/${name}.js`,
-			output: {
-				sourcemap: true,
-				format: 'iife',
-				name: 'app',
-				file: `public/build/${name}/bundle.js`
-			},
-			plugins: [
-				svelte({
-					compilerOptions: {
-						dev: !production
-					}
-				}),
-				css({ output: `bundle.css` }),
-		
-				resolve({
-					browser: true,
-					dedupe: ['svelte']
-				}),
-				commonjs(),
-		
-				// In dev mode, call `npm run start` once
-				// In flask mode, dont serve
-				// the bundle has been generated
-				!production && !flask && serve(),
-				
-				!production && (singleBundle
-					? livereload(`public`)
-					: livereload(`public/build/${name}`)),
-		
-				// If we're building for production (npm run build
-				// instead of npm run dev), minify
-				production && terser()
-			],
-			watch: {
-				clearScreen: false
-			},
-		}
-
-		configs.push(inst)
-	}
+	const configs = bundles.map((name) => create({name, flask, singleBundle}))
 
 	return configs;
 }
-
-// export default {
-// 	input: 'src/main.js',
-// 	output: {
-// 		sourcemap: true,
-// 		format: 'iife',
-// 		name: 'app',
-// 		file: 'public/build/bundle.js'
-// 	},
-// 	plugins: [
-// 		svelte({
-// 			compilerOptions: {
-// 				// enable run-time checks when not in production
-// 				dev: !production
-// 			}
-// 		}),
-// 		// we'll extract any component CSS out into
-// 		// a separate file - better for performance
-// 		css({ output: 'bundle.css' }),
-
-// 		// If you have external dependencies installed from
-// 		// npm, you'll most likely need these plugins. In
-// 		// some cases you'll need additional configuration -
-// 		// consult the documentation for details:
-// 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-// 		resolve({
-// 			browser: true,
-// 			dedupe: ['svelte']
-// 		}),
-// 		commonjs(),
-
-// 		// In dev mode, call `npm run start` once
-// 		// the bundle has been generated
-// 		!production && serve(),
-
-// 		// Watch the `public` directory and refresh the
-// 		// browser on changes when not in production
-// 		!production && livereload('public'),
-
-// 		// If we're building for production (npm run build
-// 		// instead of npm run dev), minify
-// 		production && terser()
-// 	],
-// 	watch: {
-// 		clearScreen: false
-// 	}
-// };
